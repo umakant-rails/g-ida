d6d696ae6199e098adc14ad6c2c14346df40e280
umakant-rails <rajpootumakant@gmail.com>
2014-06-14 16:35:57 +0530

    quick fixes

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Gida::Application.initialize!
-
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.gmail.com',
-  :domain         => 'mail.google.com',
-  :port           => 1025,
-  :user_name      => 'rajput.umakant4@gmail.com',
-  :password       => 'umakant05',
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
