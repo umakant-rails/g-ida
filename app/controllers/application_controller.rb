89c6567fc5d20c18f9a7e74480f97c3deca5b9e4
umakant-rails <rajpootumakant@gmail.com>
2015-03-17 20:33:02 +0530

    add admin and client layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  layout :set_layout
   def after_sign_in_path_for(resource)
     if resource.role.present? && resource.role.name.eql?('Admin')
       responses_path
@@ -18,5 +18,14 @@ class ApplicationController < ActionController::Base
       super
     end
   end
-  
+
+  private
+  def set_layout
+    if current_user.present?
+      role = current_user.role.name
+      "#{role.downcase}_layout"
+    else
+      "application"
+    end
+  end
 end
