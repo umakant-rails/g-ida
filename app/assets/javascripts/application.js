558d0d297eee4854624d2759e854bf27085b290b
umakant-rails <rajpootumakant@gmail.com>
2015-03-18 10:04:14 +0530

    add new css and images

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,6 @@
 //= require jquery
+//= require js/jquery.nanoscroller/jquery.nanoscroller.js
+//= require js/behaviour/general.js
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap
