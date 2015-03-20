0851611d4e66736a3175d7260a5b9e31bd1b14f3
umakant-rails <rajpootumakant@gmail.com>
2015-03-20 20:16:08 +0530

    resolve js issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,8 @@
 //= require jquery
-//= require js/jquery.nanoscroller/jquery.nanoscroller.js
-//= require js/behaviour/general.js
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap
 //= require jquery_nested_form
+//= require js/behaviour/general.js
+//= require js/jquery.nanoscroller/jquery.nanoscroller.js
 //= require_tree .
